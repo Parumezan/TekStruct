46f01668a9d0487498e85a40d15778cf594e5136
Parumezan <jbbrocherie@gmail.com>
2021-02-14 16:23:49 +0100

    fix: my_putnbr(-2147483648)

--- lib/my_putnbr.c
+++ lib/my_putnbr.c
@@ -11,17 +11,19 @@ void my_putnbr(int nb)
 {
     int check = 1;
 
-    if (nb == -2147483648)
+    if (nb == -2147483648) {
         my_putstr("-2147483648");
-    if (nb < 0) {
-        my_putchar('-');
-        nb *= -1;
-    }
-    while ((nb / check) > 9) {
-        check *= 10;
-    }
-    while (check != 0) {
-        my_putchar('0' + (nb / check % 10));
-        check /= 10;
+    } else {
+        if (nb < 0) {
+            my_putchar('-');
+            nb *= -1;
+        }
+        while ((nb / check) > 9) {
+            check *= 10;
+        }
+        while (check != 0) {
+            my_putchar('0' + (nb / check % 10));
+            check /= 10;
+        }
     }
 }
