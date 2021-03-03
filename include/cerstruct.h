82276441d8590d7ee9094f189e3f655615fcd616
Parumezan <jbbrocherie@gmail.com>
2021-03-03 14:10:54 +0100

    Upd/Add: LICENSE apache2 | remake of include *.h

--- include/cerstruct.h
+++ include/cerstruct.h
@@ -8,10 +8,16 @@
 #ifndef CERSTRUCT_H_
 #define CERSTRUCT_H_
 
-void my_putchar(char c);
-void my_putnbr(int nb);
-void my_putstr(char *str);
-int my_strlen(char const *str);
-int my_convert(char *str);
+//Defines:
+    //Error:
+//Includes:
+//Structures:
+//Prototypes:
+    //MiniLib (/lib)
+        void my_putchar(char c);
+        void my_putnbr(int nb);
+        void my_putstr(char *str);
+        int my_strlen(char *str);
+        int my_convert(char *str);
 
 #endif /* !CERSTRUCT_H_ */
