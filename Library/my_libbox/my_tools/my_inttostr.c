b66c72c70d7da5876d5aae2b15561104a56bf4b0
Parumezan <jbbrocherie@gmail.com>
2021-06-21 18:50:39 +0200

    Upd: Library: my_libbox: my_inttostr.c compact int to str

--- Library/my_libbox/my_tools/my_inttostr.c
+++ Library/my_libbox/my_tools/my_inttostr.c
@@ -7,37 +7,26 @@
 
 #include "my_libbox.h"
 
-static char *fill_intstr(int cpt, int save, int pastille)
-{
-    char *str = my_calloc((sizeof(char) * (cpt + 2)), 0);
-
-    if (!str)
-        return NULL;
-    for (; cpt > -1; cpt--) {
-        str[cpt] = ((save % 10) + 48);
-        save /= 10;
-    }
-    if (pastille == 1)
-        str[0] = '-';
-    return str;
-}
-
 char *my_inttostr(int nb)
 {
-    int save = nb;
-    int pastille = 0;
     int cpt = 0;
-
+    int save = nb;
+    int size = 0;
+    int check = 0;
+    char *str = NULL;
+    
     if (nb < 0) {
         save *= -1;
-        pastille = 1;
+        size = 1;
     }
-    for (; save > 0; save /= 10, cpt++);
-    cpt--;
-    save = nb;
-    if (pastille == 1) {
-        cpt += 1;
-        save = nb * -1;
-    }
-    return fill_intstr(cpt, save, pastille);
+    for (int check = save; check > 0; check /= 10, cpt++);
+    size += cpt;
+    if (!(str = my_calloc(sizeof(char) * (size + 1), 0)))
+        return NULL;
+    if (size > cpt)
+        check += 1;
+    for (check += cpt - 1; check >= 0; check--, save /= 10)
+        str[check] = ((save % 10) + 48);
+    size > cpt ? str[0] = '-' : 0;
+    return str;
 }
