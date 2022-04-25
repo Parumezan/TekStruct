a70b553ae161acec0c2d053f019bac3ab601afd9
Parumezan <jbbrocherie@gmail.com>
2022-04-25 10:15:59 +0200

    Upd: Assembleur Makefile && Simple Refactor

--- Simple/include/tekstruct.h
+++ Simple/include/tekstruct.h
@@ -6,7 +6,7 @@
 */
 
 #ifndef TEKSTRUCT_H_
-#define TEKSTRUCT_H_
+    #define TEKSTRUCT_H_
 
 //Defines:
     //Error:
@@ -15,12 +15,5 @@
     #include <stdlib.h>
 //Structures:
 //Prototypes:
-    //MiniLib (/lib)
-        void my_putchar(char c);
-        void my_putnbr(int nb);
-        void my_putstr(char *str);
-        void my_putarray(char **array);
-        int my_strlen(char *str);
-        int my_convert(char *str);
 
 #endif /* !TEKSTRUCT_H_ */
